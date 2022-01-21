d774c5c1be7b63b71d296fb85adbb472e70e1ba2
Konstantin Balakirev <kbalakirev@gmail.com>
2022-01-21 14:13:02 -0600

    switching horses

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
+const { addPage, editPage, main, userList, userPages, wikiPage } = require('./views');
+
+const PORT = 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
 app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+    res.send(main(''));
+});
 
-
+app.listen(PORT, () => {
+    console.log(`Listeting to port: ${PORT}`);
+})
